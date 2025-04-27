26e0106134499321411533b5911469ed2ebfc623
Fracypakes <10623777+fracypakes@users.noreply.github.com>
2025-04-27 18:56:47 +0800

    update tokenlist

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://bnbpunks.com/list.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://octopusswap.xyz/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
